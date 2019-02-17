dd68ee2b2aad04f84f1051c3017dce202eb8394c
ljubomirsinadinovski <ljupce995@gmail.com>
2019-02-17 14:58:49 +0100

    modified backend according to requirements

--- backend-nodejs/routes/routes.js
+++ backend-nodejs/routes/routes.js
@@ -3,22 +3,31 @@ const pool = require('../data/config');
 const router = app => {
 
     // Display all users
-    app.get('/users', (request, response) => {
-        pool.query('SELECT * FROM Users', (error, result) => {
+    app.get('/vehicles', (request, response) => {
+        pool.query(
+            `SELECT vin, reg_number, Users.customer_name, Users.customer_address FROM Vehicles 
+            INNER JOIN Users
+            ON Users.user_id = Vehicles.car_owner_id;`,
+            (error, result) => {
            if (error) {
-            console.log("ERROR IN USERS QUERY");
+            console.log("ERROR IN fetching Vehicles QUERY");
             throw error;
            }
            response.send(result);
         });
     });
 
-    // Display a single user by ID
+    // Display vehicles by user by ID
     app.get('/users/:id', (request, response) => {
         const id = request.params.id;
-        pool.query('SELECT * FROM Users WHERE user_id = ?', id, (error, result) => {
+        pool.query(
+                `SELECT vin, reg_number, Users.customer_name, Users.customer_address FROM Vehicles 
+                INNER JOIN Users
+                ON Users.user_id = Vehicles.car_owner_id
+                WHERE Users.user_id = ?;`, id,
+                 (error, result) => {
             if (error) {
-                console.log("ERROR IN USER QUERY");
+                console.log("ERROR IN fetching Vehicles QUERY");
                 throw error;
             }
             response.send(result);
@@ -26,15 +35,16 @@ const router = app => {
     });
 
     // Add a new user
-    app.post('/users', (request, response) => {
-        pool.query('INSERT INTO Users SET ?', request.body, (error, result) => {
-            if (error) {
-                console.log("ERROR IN USER INSERTION");
-                throw error;
-            }
-            response.status(201).send(`User added with ID: ${result.insertId}`);
-        });
-    });
+    // app.post('/users', (request, response) => {
+    //     pool.query('INSERT INTO Users SET ?', request.body, (error, result) => {
+    //         if (error) {
+    //             console.log("ERROR IN USER INSERTION");
+    //             throw error;
+    //         }
+    //         response.status(201).send(`User added with ID: ${result.insertId}`);
+    //     });
+    // });
+
 }
 
 module.exports = router;
